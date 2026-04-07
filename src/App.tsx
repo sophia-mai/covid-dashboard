f7ab0d59df528708e1177ea32d55de11a36bd261
unknown <sophiathanhmai@gmail.com>
2026-04-07 05:51:13 -0400

    dashboard-01: Install and configure shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
-    <div className="flex min-h-screen items-center justify-center text-3xl">
-      Covid Dashboard
+    <div className="flex min-h-screen items-center justify-center">
+      <Button>Click me</Button>
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import path from "path";
+import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+
 // https://vite.dev/config/
 export default defineConfig({
-   base: "/covid-dashboard/",
+  base: "/covid-dashboard/",
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
